e3217acb979bc0e46fe4a2560a0e09b4f86146a6
Jon Distad <jon.distad@gmail.com>
2011-06-08 13:54:54 -0700

    Move negate-and-optionize out of crazy-processing*

--- src/conch/fnparse_ext.clj
+++ src/conch/fnparse_ext.clj
@@ -1,36 +1,38 @@
 (ns conch.fnparse-ext
   (:use name.choi.joshua.fnparse))
 
+(def negate-and-optionize
+  (complex [neg? (opt (lit \!))
+            _ (lit \<)
+            name (rep+ (except anything (lit \>)))
+            _ (lit \>)
+            rep-val (alt (lit \+)
+                         (lit \*)
+                         (lit \?)
+                         emptiness)]
+           (let [sym (symbol (str "<" (apply str name) ">"))
+                 negated-form (if neg?
+                                `(except anything ~sym)
+                                sym)
+                 optionized-form (cond
+                                  (= \? rep-val)
+                                  `(opt ~negated-form)
+                                  (= \+ rep-val)
+                                  `(rep+ ~negated-form)
+                                  (= \* rep-val)
+                                  `(rep* ~negated-form)
+                                  :else
+                                  negated-form)]
+             optionized-form)))
+
 (defn crazy-processing* [forms]
   (for [form forms]
     (if (seq? form)
       (crazy-processing* form)
-      (rule-match
-       (complex [neg? (opt (lit \!))
-                 _ (lit \<)
-                 name (rep+ (except anything (lit \>)))
-                 _ (lit \>)
-                 rep-val (alt (lit \+)
-                              (lit \*)
-                              (lit \?)
-                              emptiness)]
-                (let [sym (symbol (str "<" (apply str name) ">"))
-                      negated-form (if neg?
-                                     `(except anything ~sym)
-                                     sym)
-                      optionized-form (cond
-                                       (= \? rep-val)
-                                       `(opt ~negated-form)
-                                       (= \+ rep-val)
-                                       `(rep+ ~negated-form)
-                                       (= \* rep-val)
-                                       `(rep* ~negated-form)
-                                       :else
-                                       negated-form)]
-                  optionized-form))
-       (fn [_] form)
-       (fn [_ _] form)
-       {:remainder (name form)}))))
+      (rule-match negate-and-optionize
+                  (fn [_] form)
+                  (fn [_ _] form)
+                  {:remainder (name form)}))))
 
 (defmacro <> [& rules]
   `(apply eval (crazy-processing* '~rules)))
